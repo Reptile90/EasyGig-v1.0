cfa579f4391582d763992554071c892f4c432fdb
Reptile90 <stefano.reali1990@gmail.com>
2026-03-21 01:27:33 +0100

    aggiunto endpoint per la ricerca degli artisti

--- backend/app/api/routes/artist.py
+++ backend/app/api/routes/artist.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Optional
+
+from fastapi import APIRouter, Depends, HTTPException,Query
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
-from app.models.models import Person, PersonType, Band,pers_band
+from app.models.models import Person, PersonType, Band,pers_band,Genre,City
 from app.api.routes.auth import get_current_user,get_db
 from app.schemas.schemas import ArtistUpdate, BandUpdate
 
@@ -31,8 +34,6 @@ def update_artist(
     db.refresh(current_user)
     return current_user
 
-
-router = APIRouter(prefix="/artists", tags=["Artists"])
 
 
 @router.put("/me/band")
@@ -75,3 +76,44 @@ def update_artist_band(
     db.refresh(band)
 
     return band
+
+
+@router.get("/artists")
+def search_artists(
+    artist:Optional[str] = Query(None, description="Nome artista o band"),
+    genere_id:Optional[int] = Query(None, description="ID del genere musicale"),
+    citta: Optional[str] = Query(None, description="Nome della città"),
+    categoria:Optional[str] = Query(None, description="inedita, tribute Band, cover Band"),
+    db:Session = Depends(get_db)
+
+):
+    # Base query per le Band
+    query_band = db.query(Band).join(Genre)
+    
+    #Filtri per le Band
+    if artist:
+        query_band = query_band.filter(Band.nome.ilike(f"%{artist}%"))
+    if genere_id:
+        query_band = query_band.filter(Band.genere_id == genere_id)
+    if categoria:
+        query_band = query_band.filter(Band.categoria == categoria)
+        
+    if citta:
+        query_band = query_band.join(Band.band_list).join(Person).join(City).filter(City.nome.ilike(f"%{citta}%"))
+        
+    results_bands = query_band.all()
+    
+    query_artists = db.query(Person).filter(Person.tipo_utente == PersonType.artista)
+    if artist:
+        query_artists = query_artists.filter(or_(Person.nome.ilike(f"%{artist}%"), Person.cognome.ilike(f"%{artist}%")))
+    if genere_id:
+        query_artists = query_artists.filter(Person.genere_id == genere_id)
+    if citta:
+        query_artists = query_artists.join(City).filter(City.nome.ilike(f"%{citta}%"))
+
+    results_artists = query_artists.all()
+
+    return {
+        "bands": results_bands,
+        "solo_artists": results_artists
+    }
